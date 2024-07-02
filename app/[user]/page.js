6663bf866608b9a8f9e48632ce7bf31d3a9dbb11
arpit5492 <arpitmallick99@gmail.com>
2024-07-02 16:34:11 -0700

    Offline and online data showing

--- app/[user]/page.js
+++ app/[user]/page.js
@@ -1,12 +1,74 @@
 "use client";
 
 import { useParams } from "next/navigation";
+import Link from "next/link";
+import { supabase } from "@/lib/supabase/client";
+import { useEffect, useState } from "react";
 
 export default function User() {
   const { user } = useParams();
+  // console.log(user);
+  const [userData, setUser] = useState({});
+  const fetchSinData = async () => {
+    const { data, error } = await supabase
+      .from("channel")
+      .select("*")
+      .eq("username", user);
+
+    if (data && data.length > 0) {
+      const [obj] = data;
+      setUser(obj);
+    }
+  };
+
+  useEffect(() => {
+    document.title = `${user} - Twitch`;
+    fetchSinData();
+  }, []);
+
+  console.log(userData);
   return (
-    <div>
-      <p>Hello {user}</p>
+    <div className="mt-6 h-screen mx-6 flex justify-start">
+      <div
+        style={{ height: "580px", width: "920px" }}
+        className="mx-6 rounded-lg shadow-xl flex flex-col justify-between p-3"
+      >
+        {userData?.is_streaming ? (
+          <div className="mb-3">
+            <iframe
+              height={"500px"}
+              width={"900px"}
+              className="rounded-lg"
+              src="https://www.youtube.com/embed/jfKfPfyJRdk"
+            ></iframe>
+          </div>
+        ) : (
+          <div
+            style={{
+              position: "relative",
+              top: "40%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+            className="mb-3 flex justify-center"
+          >
+            <p className="p-2 text-white flex justify-center font-bold rounded-lg bg-purple-800">
+              Offline
+            </p>
+          </div>
+        )}
+        <div className="flex flex-col justify-end">
+          <div className="flex justify-center items-center">
+            <button className="font-bold rounded-lg p-2 bg-purple-800 hover:bg-purple-900 hover:text-white">
+              <p>Follow</p>
+            </button>
+            <button className="font-bold ml-4 rounded-lg p-2 bg-purple-800 hover:bg-purple-900 hover:text-white">
+              <Link href={`/`}>Home</Link>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="h-fit rounded-lg p-2 font-bold shadow-lg">Chat Box</div>
     </div>
   );
 }

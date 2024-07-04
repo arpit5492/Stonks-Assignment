61d156df502f81c660c6e27c3a6b17a5efb7db0d
arpit5492 <arpitmallick99@gmail.com>
2024-07-03 22:41:04 -0700

    updated next.config.js file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
+    RESEND_API_KEY: process.env.RESEND_API_KEY,
+  },
+};
 
 export default nextConfig;
